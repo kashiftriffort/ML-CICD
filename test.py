cfdea9f9c144b260208148cab2efa74f0fad052f
Kashif Jilani <kashifjilani@Kashifs-MacBook-Pro.local>
2022-12-01 12:48:36 +0530

    Changes for test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from app import model_pred
 
 new_data = {'Age': 40,
-            'RestingBP': 150,
+            'RestingBP': 105,
             'Cholesterol': 195,
             'Oldpeak': 0.0,
             'FastingBS': 1,
